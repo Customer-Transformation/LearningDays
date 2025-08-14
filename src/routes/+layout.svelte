5fd9ea553cd9f4dec39ddb9a1ce5d8d6a594291b
Customer-Transformation <alfredberg90@gmail.com>
2025-08-14 11:44:18 +0200

    global css + layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
-	import { resolve } from "$app/paths";
+	import { onNavigate } from "$app/navigation"
+	import { resolve } from "$app/paths"
+	import "../globalStyles.css"
+
 	let { children } = $props();
+	let isMenuShown = $state(false)
+	onNavigate(() => {isMenuShown = false})
 </script>
 
-<nav>
-	<!-- svelte-ignore a11y_consider_explicit_label -->
-	<a href={resolve("/")}>Home</a>
-	<a href={resolve("/contact")}>Contact</a>
-	<a href={resolve("/schedule")}>Schedule</a>
-</nav>
+<header>
+	<button onpointerdown={() => isMenuShown = true}>Menu</button>
+	
+	<nav style:display={isMenuShown ? "flex" : "none"}>
+		<!-- svelte-ignore a11y_consider_explicit_label -->
+		<a href={resolve("/")}>Home</a>
+		<a href={resolve("/contact")}>Contact</a>
+		<a href={resolve("/schedule")}>Schedule</a>
+	</nav>
+</header>
+
 {@render children?.()}
+
+<style>
+	header {
+		height: 32px;
+		background-color: #222;
+	}
+
+	nav {
+		position: fixed; inset: 0 auto auto 0;
+		display: flex; flex-direction: column; gap: 8px;
+		background-color: #222;
+	}
+
+	a {
+		color: white;
+	}
+</style>
